b96f3be8eaf6b589e0fdfdb9c34b09b383790987
George Manthos <gmanthos2@hotmail.com>
2026-02-07 00:03:20 +0200

    Added ModelFactory class - preconfig'd models for easy calls

---
+++ src/models.py
@@ -0,0 +1,103 @@
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
+from xgboost import XGBClassifier
+
+from config.config import Config
+
+
+class ModelFactory:
+
+    def __init__(self):
+        self.config = Config()
+
+
+    def get_model(self, model_name):
+        models = {
+            "svm": self.get_svm(),
+            "random_forest": self.get_random_forest(),
+            "knn": self.get_knn(),
+            "logistic_regression": self.get_logistic_regression(),
+            "gradient_boosting": self.get_gradient_boosting(),
+            "xgboost": self.get_xgboost(),
+            "mlp": self.get_mlp()
+        }
+
+        if model_name not in models:
+            raise ValueError(f"Invalid model name {model_name}")
+
+        return models[model_name]
+
+    def get_svm(self):
+        return SVC(
+            kernel = 'rbf',
+            C = 10,
+            gamma = "scale",
+            random_state = self.config.RANDOM_STATE
+        )
+
+    def get_random_forest(self):
+        return RandomForestClassifier(
+            n_estimators=200,
+            max_depth=20,
+            min_samples_split=5,
+            random_state=self.config.RANDOM_STATE,
+            n_jobs=-1
+        )
+
+    def get_knn(self):
+        return KNeighborsClassifier(
+            n_neighbors=5,
+            weights='distance',
+            n_jobs=-1
+        )
+
+    def get_logistic_regression(self):
+        return LogisticRegression(
+            max_iter=1000,
+            random_state=self.config.RANDOM_STATE
+        )
+
+    def get_gradient_boosting(self):
+        return GradientBoostingClassifier(
+            n_estimators=100,
+            learning_rate=0.1,
+            max_depth=5,
+            random_state=self.config.RANDOM_STATE
+        )
+
+    def get_xgboost(self):
+        return XGBClassifier(
+            n_estimators=400,
+            learning_rate=0.05,
+            max_depth=6,
+            subsample=0.9,
+            colsample_bytree=0.9,
+            random_state=self.config.RANDOM_STATE,
+            n_jobs=-1,
+            objective="multi:softprob",
+            num_class=self.config.NUM_CLASSES,
+            eval_metric="mlogloss"
+        )
+
+    def get_mlp(self):
+        return MLPClassifier(
+            hidden_layer_sizes=(128,64),
+            activation="relu",
+            max_iter=500,
+            random_state=self.config.RANDOM_STATE
+        )
+
+    def get_all_models(self):
+
+        return {
+            "svm": self.get_svm(),
+            "random_forest": self.get_random_forest(),
+            "knn": self.get_knn(),
+            "logistic_regression": self.get_logistic_regression(),
+            "gradient_boosting": self.get_gradient_boosting(),
+            "xgboost": self.get_xgboost(),
+            "mlp": self.get_mlp()
+        }
